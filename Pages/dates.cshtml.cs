5f93ba10c9b14ff874363458aded6e70892469c9
VGD-A5\pc 5 <rhermogino@csmbataan.ph.education>
2022-09-22 00:06:50 -0700

    prelim activity 1

---
+++ Pages/dates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RACHEL.DateCalculator_.Pages
+{
+    public class datesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
